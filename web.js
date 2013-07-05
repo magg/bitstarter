bb402e33ff436cd141ba7ce9b2fede75df25c6e7
Miguel Gonzalez <maggonzz@gmail.com>
2013-07-05 00:42:33 -0500

    from buffer

--- web.js
+++ web.js
@@ -2,13 +2,10 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
-var data = fs.readFileSync("index.html");
-
-var buffer = new Buffer(data.length);
+var buffer = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
